64a2b62bd0a976ae703b2a820df5583cd12e1777
Marta Fagúndez <55920614+MartaFagundez@users.noreply.github.com>
2025-09-14 18:30:48 +0000

    feat: create, read and delete todos

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,62 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [newTodo, setNewTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const addTodo = (e) => {
+    if (e.code === "Enter" && e.target.value.trim() !== "") {
+      setTodoList([...todoList, newTodo]);
+      setNewTodo("");
+    }
+  };
+
+  const deleteTodo = (index) => {
+	  setTodoList(todoList.filter((todo, i) => i !== index))
+  }
+
+  return (
+    <div className="container-fluid d-flex flex-column align-items-center">
+      <h1>todos</h1>
+      <div
+        className="d-flex flex-column align-items-start w-100"
+        style={{ maxWidth: "550px" }}
+      >
+        <form onSubmit={handleSubmit} className="w-100">
+          <input
+            type="text"
+            name="newTodo"
+            id="newTodo"
+            placeholder="What needs to be done?"
+            className="w-100"
+            value={newTodo}
+            onChange={(e) => {
+              setNewTodo(e.target.value);
+            }}
+            onKeyDown={addTodo}
+          />
+        </form>
+        <ul className="list-group list-group-flush w-100 border">
+          {todoList.map((todo, index) => {
+            return (
+              <li className="list-group-item d-flex" key={index}>
+                {todo} <button className="text-danger ms-auto btn hidden-button" onClick={() => deleteTodo(index)}>X</button>
+              </li>
+            );
+          })}
+        </ul>
+        {
+          todoList.length > 0 &&
+          <p className="form-text">{`${todoList.length} ${todoList.length === 1 ? "item" : "items"} left`}</p>
+        }
+      </div>
+    </div>
+  );
 };
 
 export default Home;
